58ea389b49865fd05de8c788a05095ea5b118d4d
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2022-12-19 14:21:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const paragraph = document.createElement('p');
+  // paragraph.textContent = '...';
+  // document.getElementById('root').appendChild(paragraph);
+  // document.getElementById('root').innerHTML = 'HELLO!'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        <h2>start</h2>
+       <p>2</p>
+      </header> */}
+        <h2>start</h2>
+        <p>2</p>
     </div>
   );
 }
